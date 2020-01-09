e7b37ed6ef1972af4d070923b5d46a0d09574159
Dmitrij <grydmitrij@gmail.com>
2020-01-09 22:26:02 +0300

    Начал делать страницу Сведения об ОО, создал доп навигацию - переделал скрипт загрузки навигации

--- js/loadMenu.js
+++ js/loadMenu.js
@@ -1,25 +1,71 @@
 $("#banner").load("banner.html");
 $("nav.header").load("menu.html");
 
-function whereIs()
+//установка текущего тэга li в основном меню
+function setHeaderCurrentLi()
 {
-	let bodyId = $("body").attr("id");
-	let name = getBodyName(bodyId);
-	if (name.length == 0)
+	let name = getBodyName();
+	if (name == "")
 		return;
-	let a = $("nav").find("a:contains('" + name + "')");
-	a.attr("onclick", "return false;");
-	let li = a.parent("li");
-	li.addClass("here");
+	let li = getNavLiWhereIs("nav.header", name);
+	setLiHere(li);
 }
 
-function getBodyName(id)
+function setLeftNavCurrentLi()
+{
+	let name = getNavName();
+	if (name == "")
+		return;
+	let li = getNavLiWhereIs("nav.left", name);
+	setLiHere(li);
+}
+
+//возвращает текущий тег li из навигации
+function getNavLiWhereIs(navSelector, text)
+{
+	if (navSelector.length == 0)
+		return;
+	let a = $(navSelector).find("a:contains('" + text + "')");
+	return a.parent("li");
+}
+
+function setLiHere(li)
+{
+	li.addClass("here");
+	let a = li.find("a");
+	a.attr("onclick", "return false;")
+}
+
+function getBodyName()
+{
+	let bodyId = $("body").attr("id");
+	return getBodyFromId(bodyId);
+}
+
+function getNavName()
+{
+	let bodyId = $("body").attr("id");
+	return getGeneralInfoName(bodyId);
+}
+
+function getBodyFromId(id)
 {
 	let name = "";
 	switch(id) {
 		case "home":
 			name = "Главная";
 			break;
+		case "generalInfo":
+			name = "Сведения об ОО";
+			break;
+	}
+	return name;
+}
+
+function getGeneralInfoName(id)
+{
+	let name = "";
+	switch(id) {
 		case "generalInfo":
 			name = "Основные сведения";
 			break;
